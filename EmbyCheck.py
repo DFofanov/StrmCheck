792c2f9c94ec1c8bebbd1e0908210f8ddf926730
Dmitriy Fofanov <DFofanov@gmail.com>
2021-05-16 22:53:31 +0300

    Added logging algorithm

--- EmbyCheck.py
+++ EmbyCheck.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import fnmatch,os
 import json
-from pathlib import Path
 
 from subprocess import PIPE
 from subprocess import Popen
 from subprocess import TimeoutExpired
 
+import logging                                                                                                                                                                          
+import time
+from colorama import Fore
+
+from pathlib import Path
+
 directory = r'/Users/fofanov.dmitry/Project/Emby/111'
+AceServer = r'10.0.10.6'
+AceUrlSrv = r'http://127.0.0.1:6878/ace/getstream?infohash='
+TorrUrlSrv = r'http://ace.srv:8090/torrent/play?link='
 
 PROBE_COMMAND = (
     'ffprobe -hide_banner -show_streams -select_streams v '
     '-of json=c=1 -v quiet'
 )
 
+ok_msg = Fore.GREEN + "OK!  " + Fore.RESET                                                                                                                                              
+fail_msg = Fore.RED + "FAIL " + Fore.RESET   
+
+logging.basicConfig(                                                                                                                                                                    
+    level=logging.WARNING,                                                                                                                                                              
+    format='[%(asctime)s] %(levelname)s: %(message)s',                                                                                                                                  
+    datefmt='%Y-%m-%d %I:%M:%S')                                                                                                                                                        
+log = logging.getLogger('ParseRuTor')                                                                                                                                                   
+log.setLevel(logging.DEBUG)  
+
+# Getting the frame width of a video picture
 def probe(url, timeout=None):
     """Invoke probe to get stream information."""
     outs = None
@@ -31,6 +50,7 @@ def probe(url, timeout=None):
             print(exc)
     return None
 
+# We get by frame size, video quality
 def get_category(width):
     category = None
     if (width <= 256): category = '144p'
@@ -57,6 +77,8 @@ def deleteDir(dirPath):
         os.rmdir(d)
     os.rmdir(dirPath)
 
+# Checking the folder if there is a file with the specified extension, 
+# if not, then delete the folder
 def CheckDir(root,files,ext):
     contains_other_ext = 0
     for file in files:
@@ -65,26 +87,32 @@ def CheckDir(root,files,ext):
     if contains_other_ext == 0:
         deleteDir(root)    
 
+# Main process
 def main():
     os.chdir(directory)
+    i = 0
     del_filenames = []
+    # We get the number of files in the directory
+    count = len([files for _, _, files in os.walk(directory) for file in files if file.endswith('.strm')])
     for root, dirs, files in os.walk(".", topdown=False):
         del_filenames.clear()
         for name in filter(lambda x: x.endswith('.strm'), files):
             filename = os.path.join(directory, root.replace('./',''), name)
+            i = i + 1
             url = ''
             with open(filename) as file:
                 lines = file.readlines()
                 for line in lines:
                     line = line.strip()
-                    if ('10.0.10.6' in line) or (':6878/ace/getstream?infohash=' in line):
-                        url = line.replace('10.0.10.6','ace.srv').replace('http://127.0.0.1:6878/ace/getstream?infohash=','http://ace.srv:8090/torrent/play?link=')
+                    if (AceServer in line) or (AceUrlSrv in line):
+                        url = line.replace(AceServer,'ace.srv').replace(AceUrlSrv,TorrUrlSrv)
                         probe_url = url
             if len(url) > 0:
                 with open(filename, 'w') as file:
                     file.writelines(url)
             else: probe_url = line            
             width = None
+            t1 = time.time()  
             obj_probe = probe(probe_url,30)
             if obj_probe != None and len(obj_probe) > 0:
                 if len(obj_probe["streams"]) > 0:
@@ -95,25 +123,36 @@ def main():
                 del_filenames.append(os.path.basename(filename))
                 if os.path.isfile(os.path.splitext(filename)[0]+'.nfo'):
                     os.remove(os.path.splitext(filename)[0]+'.nfo')
-                print('DELETE - ' + filename)
+                log_msg = str("Rec: %-4s of %-4s, FileName: %-60s \t\t" % (int(i), int(count)-1, os.path.basename(filename)))
+                log_msg += "%s (%s)" % (fail_msg, 'DELETE FILE')
+                print(log_msg)
             else:
                 try:
                     upd_filename = os.path.splitext(filename)[0].replace('-CamRip','-'+get_category(width)).replace('-SD','-'+get_category(width)).replace('-HD','-'+get_category(width)).replace('-FULL HD','-'+get_category(width)).replace('-4K UHD','-'+get_category(width)) + os.path.splitext(filename)[1]
                     if not ('(nnm)' in upd_filename):
                         upd_filename = os.path.splitext(upd_filename)[0] + ' (nnm)' + os.path.splitext(upd_filename)[1]
                     os.rename(filename, upd_filename)
-                    print('OK - ' + upd_filename)
-                except:
-                    pass
-                    print('ERROR - ' + filename)
+                    log_msg = str("Rec: %-4s of %-4s, FileName: %-60s \t\t" % (int(i), int(count)-1, os.path.basename(upd_filename)))
+                    t2 = time.time() 
+                    log_msg += ok_msg + "Response time: %d" % ( int((t2-t1)*1000)) 
+                    print(log_msg)
+                except Exception as e:
+                    log_msg = str("Rec: %-4s of %-4s, FileName: %-60s \t\t" % (int(i), int(count)-1, os.path.basename(filename)))
+                    log_msg += "%s (%s)" % (fail_msg, str(e))
+                    print(log_msg)
+                    pass                
             if os.path.isfile(os.path.splitext(filename)[0].split('-')[0] + '.nfo'):
                 os.remove(os.path.splitext(filename)[0].split('-')[0] + '.nfo')
-        if dirs == []:
+        if (dirs == [] and len(del_filenames) != 0):
             for del_filename in del_filenames:
                 files.remove(del_filename)
             CheckDir(root,files,('.strm'))
+            log_msg = str("DELETE PATH: %s" % (root))
+            print(log_msg)
     if root != "." and os.listdir(root) == []:
         deleteDir(root)
+        log_msg = str("DELETE PATH: %s" % (root))
+        print(log_msg)
 
 if __name__ == '__main__':
     main()
